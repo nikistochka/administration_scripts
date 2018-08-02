7834c9bbce9ff313ec2ee11086301ecd376da27a
Nikita <34151435+nikistochka@users.noreply.github.com>
2018-08-02 11:53:12 +0300

    Create hardware_backup.py

---
+++ network_hardware/hardware_backup.py
@@ -0,0 +1,35 @@
+import getpass
+import paramiko
+import sys
+import time
+
+
+tftp_server = '192.168.1.1'
+
+with open('hardware.txt', 'r') as hardware_file:
+    for line in hardware_file:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        if not line.strip() or line[0] == '#':
+            continue
+        hardware = line.strip('\n').split(' ')
+        device, ssh_port, username, password = hardware[0], hardware[1], hardware[2], hardware[3]
+        print('*** Connecting to {}...'.format(device))
+
+        try:
+            ssh.connect(device, ssh_port, username, password)
+        except paramiko.AuthenticationException as exception:
+            print('*** Connect failed:', str(exception))
+            password = getpass.getpass('Please enter valid password for {} device:'.format(device))
+            ssh.connect(device, ssh_port, username, password)
+
+        with ssh.invoke_shell() as channel:
+            time.sleep(5)
+            channel.send('\n')
+            time.sleep(2)
+            channel.send('copy running-config tftp {} {}\n'.format(tftp_server, device + '_' + time.strftime("%Y%m%d-%H%M%S")))
+            time.sleep(10)
+            result = channel.recv(2000).decode('utf-8')
+            print(result)
+        print('All backups is done')
+        ssh.close()

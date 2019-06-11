fd77d58bc26336cfc32a55f4a057998998e80ba6
Nikita <34151435+nikistochka@users.noreply.github.com>
2019-06-11 13:47:40 +0300

    Update and rename docker_backup_owncloud_files.sh to docker_backup_owncloud.sh

---
+++ owncloud_backup/docker_backup_owncloud.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Script for backup a volume from a container
+
+CONTAINER_NAME_OWNCLOUD=owncloud_owncloud_1
+# Path to files inside Docker container:
+PATH_TO_FILES=/mnt/data
+PATH_TO_BACKUPS_FILES=/backup/owncloud_data
+# MySQL:
+CONTAINER_NAME_MYSQL=owncloud_db_1
+MYSQL_PASSWORD=password
+PATH_TO_BACKUPS_MYSQL=/backup/owncloud_mysql
+
+
+docker stop ${CONTAINER_NAME_OWNCLOUD};
+docker run --rm --volumes-from ${CONTAINER_NAME_OWNCLOUD} -v ${PATH_TO_BACKUPS_FILES}:/backup busybox tar cvf /backup/files_$(date +%Y%m%d_%H-%M-%S).tar ${PATH_TO_FILES};
+docker start ${CONTAINER_NAME_OWNCLOUD};
+docker exec ${CONTAINER_NAME_MYSQL} /usr/bin/mysqldump -u owncloud --password=${MYSQL_PASSWORD} owncloud > ${PATH_TO_BACKUPS_MYSQL}/mysql_$(date +%Y%m%d_%H-%M-%S).sql

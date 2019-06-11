9de8aee6dbb85781736623f141589f4745b5f937
Nikita <34151435+nikistochka@users.noreply.github.com>
2019-06-11 21:02:32 +0300

    Update docker_backup_owncloud.sh

--- owncloud_backup/docker_backup_owncloud.sh
+++ owncloud_backup/docker_backup_owncloud.sh
@@ -2,15 +2,16 @@
 # Script for backup a volume from a container
 
 CONTAINER_NAME_OWNCLOUD=owncloud_owncloud_1
-# Path to files inside Docker container:
+# Path to files inside Docker container
 PATH_TO_FILES=/mnt/data
 PATH_TO_BACKUPS_FILES=/backup/owncloud_data
-# MySQL:
+# MySQL
 CONTAINER_NAME_MYSQL=owncloud_db_1
+MYSQL_USERNAME=username
 MYSQL_PASSWORD=password
 PATH_TO_BACKUPS_MYSQL=/backup/owncloud_mysql
 
-# Make dir if does not exist:
+# Make dir if does not exist
 for PATH_VAR in ${PATH_TO_BACKUPS_FILES} ${PATH_TO_BACKUPS_MYSQL}
 do
   if [[ ! -d ${PATH_VAR} ]]; then
@@ -18,7 +19,10 @@ do
   fi
 done
 
+# Back up user files
 docker stop ${CONTAINER_NAME_OWNCLOUD};
 docker run --rm --volumes-from ${CONTAINER_NAME_OWNCLOUD} -v ${PATH_TO_BACKUPS_FILES}:/backup busybox tar cvf /backup/files_$(date +%Y%m%d_%H-%M-%S).tar ${PATH_TO_FILES};
 docker start ${CONTAINER_NAME_OWNCLOUD};
-docker exec ${CONTAINER_NAME_MYSQL} /usr/bin/mysqldump -u owncloud --password=${MYSQL_PASSWORD} owncloud > ${PATH_TO_BACKUPS_MYSQL}/mysql_$(date +%Y%m%d_%H-%M-%S).sql
+
+# Back up owncloud MySQL database
+docker exec ${CONTAINER_NAME_MYSQL} /usr/bin/mysqldump -u ${MYSQL_USERNAME} --password=${MYSQL_PASSWORD} owncloud > ${PATH_TO_BACKUPS_MYSQL}/mysql_$(date +%Y%m%d_%H-%M-%S).sql
